b5dcdf060866189ef64d678e5d0db475b1a654af
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-11-11 00:55:22 +0530

    added

--- Js/index-B.js
+++ Js/index-B.js
@@ -1,6 +1,7 @@
 const API_KEY = `27c773b22e75b4f9dcffc582e3d38f78`;
 const searchWeather = () =>{
     const cityName = document.getElementById("input-name").value;
+    document.getElementById("input-name").value = '';
     const url = ` https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
     fetch(url)
     .then(res => res.json())
