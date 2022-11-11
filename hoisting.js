7840cb92b423da826adfaeea9a02c21e3937ece4
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-11-11 11:41:07 +0530

    added

---
+++ hoisting.js
@@ -0,0 +1,19 @@
+/**
+  about hoisting
+ */
+function patriChai( age, earning){
+    console.log(age);
+    console.log(earning);
+    var trueAge = age + 7;
+    function canMarry(expense){
+        const remaining = remaining - expense;
+        if(remaining > 1000){
+            return true;
+        }
+        return false;
+        
+        console.log(expense);
+    }
+}
+const patriDemand = patriChai(22, 14000);
+// patriDemand.canMarry(200);
